267f2caa54e5c83117f5252a983424c3a5101427
Christopher R. Murphy <chmurph2+git@gmail.com>
2010-02-11 16:23:14 -0500

    Renamed to rails_watchr.rb

--- rails_watchr.rb
+++ rails_watchr.rb
@@ -21,19 +21,19 @@ def run_all_tests
 end
 
 def ruby_cmd
-  "time ruby -rubygems"
+  "ruby -rubygems"
 end
 
 # --------------------------------------------------
 # Watchr Rules
 # --------------------------------------------------
-watch( '.*_test\.rb')       { |m| run("#{ruby_cmd} %s" % m[0]) }                                    # watch all tests
+watch( '.*_test\.rb')               { |m| run("#{ruby_cmd} %s" % m[0]) }                            # watch all tests
 watch( '^lib/(.*)\.rb')             { |m| run("#{ruby_cmd} test/unit/%s_test.rb" % m[1]) }          # watch all libs
 watch( '^app/models/(.*)\.rb')      { |m| run("#{ruby_cmd} test/unit/%s_test.rb" % m[1]) }          # watch all models
 watch( '^app/controllers/(.*)\.rb') { |m| run("#{ruby_cmd} test/functional/%s_test.rb" % m[1]) }    # watch all controllers
 watch( '^app/helpers/(.*)\.rb')     { |m| run("#{ruby_cmd} test/unit/helpers/%s_test.rb" % m[1]) }  # watch all helpers
 watch( '^test/test_helper\.rb' )    { run_all_tests }                                               # watch test_helper
-watch( '^agents/(.*_agent)\.rb')    do |m|                                                          # watch all agents
+watch( '^agents/(.*_agent)\.rb') do |m|                                                             # watch all agents
   name = m[1].split('/').last
   name_sans_agent = name.gsub(/_agent/, '')
   run("#{ruby_cmd} agents/%s/test/unit/%s_test.rb" % [name_sans_agent, name])
