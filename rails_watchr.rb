849fa928ad718a9055230b578af12ecae34de29f
Christopher R. Murphy <chmurph2+git@gmail.com>
2010-07-05 13:01:19 -0400

    Improved rails_watchr script based on a recommended gist.
    * Growl support
    * Streamlined rules
    * Improved helper methods

--- rails_watchr.rb
+++ rails_watchr.rb
@@ -1,5 +1,10 @@
 # See http://github.com/mynyml/watchr/
-# This script was modified from http://gist.github.com/raw/189052/1fd4faf0e9edab9d8e941fd21a1a9e7f70cf5aad/specs.watchr.rb
+# This script was modified from http://gist.github.com/raw/276317/45b7ca8a20f0585acc46bc75fade09a260155a61/tests.watchr
+#
+# For this to run you’ll need to place the standard “passing.png”, “failing.png” images in a “~/.watchr_images” folder,
+# and you’ll need growlnotify in your path. It currently only handles passing and failing cases.
+# In theory you can just get rid of the growl pieces and use ‘system’ to run the commands instead of backticks,
+# but I wanted growl to notify me as I code, rather than having to watch my terminal.
 #
 # Run me with:
 #
@@ -9,35 +14,53 @@
 #
 # alias rw='watchr ~/.rails.watchr.rb'
 
+
+ENV["WATCHR"] = "1"
+system 'clear'
+
+def growl(message)
+  growlnotify = `which growlnotify`.chomp
+  title = "Watchr Test Results"
+  image = message.include?('0 failures, 0 errors') ? "~/.watchr_images/rails_pass.png" : "~/.watchr_images/rails_fail.png"
+  options = "-w -n Watchr --image '#{File.expand_path(image)}' -m '#{message}' '#{title}'"
+  system %(#{growlnotify} #{options} &)
+end
+
 def run(cmd)
   puts(cmd)
-  system(cmd)
-  puts "*** #{Time.now} ***\n\n"
+  # system(cmd)
+  `#{cmd}`
+end
+
+def run_test_file(file)
+  system('clear')
+  result = run(%Q(ruby -I"lib:test" -rubygems #{file}))
+  growl result.split("\n").last rescue nil
+  puts result
 end
 
 def run_all_tests
-  cmd = "time rake"
-  run(cmd)
+  system('clear')
+  result = run "rake test"
+  puts result
 end
 
-def ruby_cmd
-  "ruby -rubygems"
+def related_test_files(path)
+  Dir['test/**/*.rb'].select { |file| file =~ /#{File.basename(path).split(".").first}_test.rb/ }
 end
+
 
 # --------------------------------------------------
 # Watchr Rules
 # --------------------------------------------------
-watch( '.*_test\.rb')               { |m| run("#{ruby_cmd} %s" % m[0]) }                            # watch all tests
-watch( '^lib/(.*)\.rb')             { |m| run("#{ruby_cmd} test/unit/%s_test.rb" % m[1]) }          # watch all libs
-watch( '^app/models/(.*)\.rb')      { |m| run("#{ruby_cmd} test/unit/%s_test.rb" % m[1]) }          # watch all models
-watch( '^app/controllers/(.*)\.rb') { |m| run("#{ruby_cmd} test/functional/%s_test.rb" % m[1]) }    # watch all controllers
-watch( '^app/helpers/(.*)\.rb')     { |m| run("#{ruby_cmd} test/unit/helpers/%s_test.rb" % m[1]) }  # watch all helpers
-watch( '^test/test_helper\.rb' )    { run_all_tests }                                               # watch test_helper
-watch( '^agents/(.*_agent)\.rb') do |m|                                                             # watch all agents
-  name = m[1].split('/').last
-  name_sans_agent = name.gsub(/_agent/, '')
-  run("#{ruby_cmd} agents/%s/test/unit/%s_test.rb" % [name_sans_agent, name])
-end
+watch('test/test_helper\.rb')    { run_all_tests }                                             # watch test_helper
+watch('test/.*/.*_test\.rb')     { |m| run_test_file(m[0]) }                                     # watch all tests
+watch('(app/lib)/.*/.*\.rb')     { |m| related_test_files(m[0]).map {|tf| run_test_file(tf) } }  # watch app and lib folders
+# watch( '^agents/(.*_agent)\.rb') do |m|                                                        # watch all agents
+#   name = m[1].split('/').last
+#   name_sans_agent = name.gsub(/_agent/, '')
+#   run("#{ruby_cmd} agents/%s/test/unit/%s_test.rb" % [name_sans_agent, name])
+# end
 puts "Every move you make... ♪"
 
 # --------------------------------------------------
@@ -50,4 +73,15 @@ Signal.trap('QUIT') do
 end
 
 # Ctrl-C
-Signal.trap('INT') { abort("\n") }
+Signal.trap 'INT' do
+  if @interrupted then
+    @wants_to_quit = true
+    abort("\n")
+  else
+    puts "Interrupt a second time to quit"
+    @interrupted = true
+    Kernel.sleep 1.5
+    # raise Interrupt, nil # let the run loop catch it
+    run_all_tests
+  end
+end
