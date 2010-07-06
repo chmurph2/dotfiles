c9fdc2a57a7c63579eb2e5a8c539c5226ee65496
Christopher R. Murphy <chmurph2+git@gmail.com>
2010-07-06 15:41:20 -0400

    Typo in watch regex.

--- rails_watchr.rb
+++ rails_watchr.rb
@@ -53,10 +53,10 @@ end
 # --------------------------------------------------
 # Watchr Rules
 # --------------------------------------------------
-watch('test/test_helper\.rb')    { run_all_tests }                                             # watch test_helper
-watch('test/.*/.*_test\.rb')     { |m| run_test_file(m[0]) }                                     # watch all tests
-watch('(app/lib)/.*/.*\.rb')     { |m| related_test_files(m[0]).map {|tf| run_test_file(tf) } }  # watch app and lib folders
-# watch( '^agents/(.*_agent)\.rb') do |m|                                                        # watch all agents
+watch('test/test_helper\.rb') { run_all_tests }                                              # watch test_helper
+watch('test/.*/.*_test\.rb')  { |m| run_test_file(m[0]) }                                    # watch all tests
+watch('(app|lib)/.*/.*\.rb')  { |m| related_test_files(m[0]).map {|tf| run_test_file(tf) } } # watch app and lib folders
+# watch( '^agents/(.*_agent)\.rb') do |m|                                                      # watch all agents
 #   name = m[1].split('/').last
 #   name_sans_agent = name.gsub(/_agent/, '')
 #   run("#{ruby_cmd} agents/%s/test/unit/%s_test.rb" % [name_sans_agent, name])
