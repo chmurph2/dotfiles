71b20240fb40975aa06119ffd5343c16b5304852
Christopher R. Murphy <chmurph2+git@gmail.com>
2010-08-04 19:48:19 -0400

    Improved growl output for test results.

--- rails_watchr.rb
+++ rails_watchr.rb
@@ -35,7 +35,7 @@ end
 def run_test_file(file)
   system('clear')
   result = run(%Q(ruby -I"lib:test" -rubygems #{file}))
-  growl result.split("\n").last rescue nil
+  growl result.match(/^\d+ tests.*skips$/m).to_s
   puts result
 end
 
